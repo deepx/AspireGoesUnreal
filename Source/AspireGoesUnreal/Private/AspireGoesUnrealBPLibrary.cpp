95c023f2b9e7a1501d31663f3924a8f68b10da89
Mario 'DeepX' Staats <postmaster@deepx.de>
2024-05-17 14:55:46 +0200

    Added blank Plugin

---
+++ Source/AspireGoesUnreal/Private/AspireGoesUnrealBPLibrary.cpp
@@ -0,0 +1,14 @@
+#include "AspireGoesUnrealBPLibrary.h"
+#include "AspireGoesUnreal.h"
+
+UAspireGoesUnrealBPLibrary::UAspireGoesUnrealBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UAspireGoesUnrealBPLibrary::AspireGoesUnrealSampleFunction(float Param)
+{
+	return -1;
+}
+
